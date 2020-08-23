e533d34d08bd5426b1f6e90b2a5b83726fabcac0
Nabeil Lemar <nabeillemar@gmail.com>
2020-08-23 13:31:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line 
+  #"The line is currently empty"
+end
+
+def take_a_number(katz_deli, "Name")
+end
+
+def now_serving
+  #"There is nobody waiting to be served!"
+end
